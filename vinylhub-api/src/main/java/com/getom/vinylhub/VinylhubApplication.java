252c3361a338ceafdd6538936ed2c5986415b108
Gent Myftiu <myftiug@gmail.com>
2025-11-03 13:16:14 +0100

    Project Init

---
+++ vinylhub-api/src/main/java/com/getom/vinylhub/VinylhubApplication.java
@@ -0,0 +1,13 @@
+package com.getom.vinylhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylhubApplication.class, args);
+	}
+
+}
---
+++ vinylhub-api/src/test/java/com/getom/vinylhub/VinylhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getom.vinylhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
